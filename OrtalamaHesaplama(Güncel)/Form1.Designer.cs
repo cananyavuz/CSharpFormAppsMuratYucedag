ea5f83a8c7edaceb792836cabbda5320694a9e3e
Canan Yavuz <yavuzcanan15@gmail.com>
2022-06-02 16:56:53 +0300

    update project

--- OrtalamaHesaplama(Güncel)/Form1.Designer.cs
+++ OrtalamaHesaplama(Güncel)/Form1.Designer.cs
@@ -193,7 +193,7 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(581, 144);
             this.listBox1.TabIndex = 5;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+//            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // pictureBox1
             // 
--- OrtalamaHesaplama(Güncel)/Form1.cs
+++ OrtalamaHesaplama(Güncel)/Form1.cs
@@ -10,7 +10,7 @@ namespace OrtalamaHesaplama
         private void button1_Click(object sender, EventArgs e)
         {
 
-            // Tek satýrda da yazýlabilirdi ama rahat anlaþýlabilmesi için böyle yazdým.
+            // Tek satirda da yazilabilirdi ama rahat anlasilabilmesi icin boyle yazdim.
             int sinav1 = Convert.ToInt32(TxtVize.Text);
             int sinav2 = Convert.ToInt32(TxtFinal.Text);
             int proje = Convert.ToInt32(TxtQuiz.Text);
@@ -18,51 +18,51 @@ namespace OrtalamaHesaplama
 
 
 
-            //AA / 90 - 100 / 4.0 / Baþarýlý
-            //BA / 80 - 89 / 3.5 / Baþarýlý
-            //BB / 75 - 79 / 3.0 / Baþarýlý
-            //CB / 70 - 74 / 2.5 / Baþarýlý
-            //CC / 60 - 69 / 2.0 / Baþarýlý
-            //DC / 50 - 59 / 1.5 / Koþullu Baþarýlý - Baþarýsýz.
-            //DD / 40 - 49 / 1.0 / Baþarýsýz.
-            //FD / 30 - 39 / 0.5 / Baþarýsýz.
-            //FF / 0 - 29 / 0.0 / Baþarýsýz.
+            //AA / 90 - 100 / 4.0 / Basarili
+            //BA / 80 - 89 / 3.5 / Basarili
+            //BB / 75 - 79 / 3.0 / Basarili
+            //CB / 70 - 74 / 2.5 / Basarili
+            //CC / 60 - 69 / 2.0 / Basarili
+            //DC / 50 - 59 / 1.5 / Koþullu Basarili - Basarisiz
+            //DD / 40 - 49 / 1.0 / Basarisiz
+            //FD / 30 - 39 / 0.5 / Basarisiz
+            //FF / 0 - 29 / 0.0 / Basarisiz
 
             if (ogrenciort >= 90 && ogrenciort <= 100.0)
             {
-                listBox1.Items.Add(TxtAd + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (AA) Baþarýlý");
+                listBox1.Items.Add(TxtAd + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (AA) Basarili");
             }
             else if (ogrenciort >=  80 && ogrenciort <= 89)
             {
-                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (BA) Baþarýlý");
+                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (BA) Basarili");
             }
             else if (ogrenciort >= 75 && ogrenciort <= 79)
             {
-                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (BB) Baþarýlý");
+                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (BB) Basarili");
             }
             else if (ogrenciort >= 70 && ogrenciort <= 74)
             {
-                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (CB) Baþarýlý");
+                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (CB) Basarili");
             }
             else if (ogrenciort >= 60 && ogrenciort <= 69)
             {
-                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (CC) Baþarýlý");
+                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (CC) Basarili");
             }
             else if (ogrenciort >= 50 && ogrenciort <= 59)
             {
-                listBox1.Items.Add( TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (DC) Koþullu Baþarýlý - Baþarýsýz");
+                listBox1.Items.Add( TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (DC) Koþullu Basarili - Basarisiz");
             }
             else if (ogrenciort >= 40 && ogrenciort <= 49)
             {
-                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (DD) Baþarýsýz");
+                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (DD) Basarisiz");
             }
             else if (ogrenciort >= 30 && ogrenciort <= 39)
             {
-                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (FD) Baþarýsýz");
+                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (FD) Basarisiz");
             }
             else
             {
-                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (FF) Baþarýsýz");
+                listBox1.Items.Add(TxtAd.Text + " " + TxtSoyad.Text + " Ortalama: " + ogrenciort + " / (FF) Basarisiz");
             }
 
             TxtAd.Clear();
@@ -74,9 +74,6 @@ namespace OrtalamaHesaplama
 
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
